18dc784968015c8f721725ebf02d720672a1da57
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-04-28 23:42:57 -0300

    repository remote

--- app/src/main/java/com/example/movieapppopular/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapppopular/repository/MovieRepositoryImpl.kt
@@ -1,13 +1,17 @@
 package com.example.movieapppopular.repository
 
+import com.example.movieapppopular.data.local.LocalMovieDataSource
 import com.example.movieapppopular.data.model.MovieList
 import com.example.movieapppopular.data.remote.RemoteMovieDataSource
 
-class MovieRepositoryImpl(private val dataSource: RemoteMovieDataSource) : MovieRepository {
+class MovieRepositoryImpl(
+    private val dataSourceRemote: RemoteMovieDataSource,
+    private val dataSourceLocal: LocalMovieDataSource
+) : MovieRepository {
 
-    override suspend fun getPopularMovies(): MovieList = dataSource.getPopularMovies()
+    override suspend fun getPopularMovies(): MovieList = dataSourceRemote.getPopularMovies()
 
-    override suspend fun getTopRatedMovies(): MovieList = dataSource.getTopRatedMovies()
+    override suspend fun getTopRatedMovies(): MovieList = dataSourceRemote.getTopRatedMovies()
 
-    override suspend fun getUpcomingMovies(): MovieList = dataSource.getUpcomingMovies()
+    override suspend fun getUpcomingMovies(): MovieList = dataSourceRemote.getUpcomingMovies()
 }
